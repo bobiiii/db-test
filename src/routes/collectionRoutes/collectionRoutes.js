2ef2e434345e727a4f86daf0b4197dafbe216196
hasnain raza <hr779656@gmail.com>
2024-03-28 00:40:54 +0500

    #

--- src/routes/collectionRoutes/collectionRoutes.js
+++ src/routes/collectionRoutes/collectionRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const { collectionControllers } = require('../../controllers');
-const { adminRoutes } = require('../../middlewares');
+// const { adminRoutes } = require('../../middlewares');
 
 const upload = multer();
 
@@ -11,7 +11,7 @@ collectionRoute.get('/', collectionControllers.getCollections);
 collectionRoute.post('/add-collection', upload.any(), collectionControllers.addCollection);
 collectionRoute.get('/:collectionId', collectionControllers.getCollection);
 collectionRoute.put('/update-collection/:collectionId', upload.any(), collectionControllers.updateCollection);
-collectionRoute.delete('/delete-collection/:collectionId', adminRoutes, collectionControllers.deleteCollection);
+collectionRoute.delete('/delete-collection/:collectionId', collectionControllers.deleteCollection);
 // varieties api
 collectionRoute.post('/variety/:collectionId/add-variety', upload.any(), collectionControllers.addCollectionVariety);
 collectionRoute.put('/variety/update-variety/:varietyId', upload.any(), collectionControllers.updateCollectionVariety);
