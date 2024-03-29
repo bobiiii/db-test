f0fb2675f8ca7aff6391c5b43ae9d05d9c840caa
Babar Khan <babersdn@gmail.com>
2024-03-29 18:32:02 +0500

    bathroom  / kitchen model added

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,13 @@
 const userModel = require('./user.model');
 const blogModel = require('./blog.model');
 const collectionModel = require('./collection.model');
+const kitchenModel = require('./kitchen.model');
+const bathroomModel = require('./bathroom.model');
 
 module.exports = {
   userModel,
   blogModel,
   collectionModel,
+  kitchenModel,
+  bathroomModel,
 };
