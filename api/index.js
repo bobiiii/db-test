d6f420556b412235f9aa683eaf68d7b0cdb6f53c
bobiiii <babersdn@gmail.com>
2024-11-14 18:07:55 +0500

    cors fix

--- api/index.js
+++ api/index.js
@@ -20,6 +20,7 @@ app.use(
       'http://localhost:3000',
       'https://sharifstone.com',
       'https://www.sharifstone.com',
+      'https://sharif-dashboard-w3ml.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
