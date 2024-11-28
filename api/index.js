cf9006fb0e450689181924abf7678c7a1cc453e1
Usmanzaib356 <110809791+Usmanzaib356@users.noreply.github.com>
2024-11-28 23:31:13 +0500

    added URLS

--- api/index.js
+++ api/index.js
@@ -18,7 +18,9 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:3001',
       'https://sharifstone.com',
+      'https://master.d2u1pm0dta5ggo.amplifyapp.com',
       'https://dashboard.sharifstone.com',
       'https://sharif-stone-frontend.vercel.app',
       'https://www.sharifstone.com',
