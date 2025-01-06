8450ca5fc737b15a632a34937b3138801e0bf365
bobiiii <babersdn@gmail.com>
2025-01-06 19:25:22 +0500

    login user token fix

--- src/routes/userRoutes/usersRoutes.js
+++ src/routes/userRoutes/usersRoutes.js
@@ -5,7 +5,7 @@ const { adminRoutes } = require('../../middlewares');
 const userRoute = express.Router();
 
 userRoute.post('/add-user', adminRoutes, userControllers.addUserController);
-userRoute.post('/login-user', adminRoutes, userControllers.loginUserController);
+userRoute.post('/login-user', userControllers.loginUserController);
 userRoute.get('/get-users', adminRoutes, userControllers.getUsers);
 userRoute.put('/update-user/:userId', adminRoutes, userControllers.updateUserController);
 userRoute.delete('/delete-user/:userId', adminRoutes, userControllers.deleteUser);
